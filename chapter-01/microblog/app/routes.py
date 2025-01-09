e8bbc7a99c00a1d587106f1dee9b27148ae61fe7
spicyneutrino <107169289+spicyneutrino@users.noreply.github.com>
2025-01-09 11:08:59 -0600

    chapter-02

--- chapter-01/microblog/app/routes.py
+++ chapter-01/microblog/app/routes.py
@@ -1,14 +1,23 @@
+from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello World!"
 
-@app.route('/about')
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {"username": "Miguel"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    return render_template("index.html", user=user, posts = posts)
+
+
+@app.route("/about")
 def about():
     return "About Page"
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
     return "Contact us"
